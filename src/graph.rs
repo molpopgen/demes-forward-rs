b02aeff787787d5409f01f82069b1b7df4a5707d
Kevin R. Thornton <krthornt@uci.edu>
2022-06-20 16:07:06 -0700

    Add basic time conversion to back end. (#3)

--- src/graph.rs
+++ src/graph.rs
@@ -1,14 +1,87 @@
 use std::fmt::Debug;
 
+use crate::time::ModelTime;
 use crate::DemesForwardError;
 use crate::ForwardTime;
+use crate::IntoForwardTime;
+
+#[derive(Debug)]
+pub struct CurrentEpochData {
+    epoch: demes::Epoch,
+}
+
+impl CurrentEpochData {
+    pub fn end_time(&self) -> demes::Time {
+        self.epoch.end_time()
+    }
+}
+
+#[derive(Debug)]
+pub struct CurrentEpochs {
+    time: ForwardTime,
+    epochs: Vec<CurrentEpochData>,
+}
+
+impl CurrentEpochs {
+    fn iter(&self) -> std::slice::Iter<CurrentEpochData> {
+        self.epochs.iter()
+    }
+}
 
 pub struct ForwardGraph {
     graph: demes::Graph,
+    pub(crate) model_times: ModelTime, // FIXME: this should be private to this module.
+    child_epochs: Option<CurrentEpochs>,
+    parent_epochs: Option<CurrentEpochs>,
 }
 
 impl ForwardGraph {
-    pub fn new<F: Into<ForwardTime> + Debug>(
+    fn calculate_current_epochs(
+        &mut self,
+        generation_time: ForwardTime,
+    ) -> Result<Option<CurrentEpochs>, DemesForwardError> {
+        let backwards_time = self.model_times.convert(generation_time)?;
+        match backwards_time {
+            Some(backwards_time) => {
+                let mut e = vec![];
+                for deme in self.graph.demes() {
+                    if backwards_time < deme.start_time() {
+                        if let Some(epoch) = deme
+                            .epochs()
+                            .iter()
+                            .find(|epoch| epoch.end_time() <= backwards_time)
+                        {
+                            e.push(CurrentEpochData { epoch: *epoch });
+                        }
+                    }
+                }
+                Ok(Some(CurrentEpochs {
+                    time: generation_time,
+                    epochs: e,
+                }))
+            }
+            None => Ok(None),
+        }
+    }
+
+    fn update_parental_deme_epochs(
+        &mut self,
+        parental_generation_time: ForwardTime,
+    ) -> Result<(), DemesForwardError> {
+        self.parent_epochs = self.calculate_current_epochs(parental_generation_time)?;
+        Ok(())
+    }
+
+    fn update_child_deme_epochs(
+        &mut self,
+        parental_generation_time: ForwardTime,
+    ) -> Result<(), DemesForwardError> {
+        let child_generation_time = ForwardTime::from(parental_generation_time.value() + 1.);
+        self.child_epochs = self.calculate_current_epochs(child_generation_time)?;
+        Ok(())
+    }
+
+    pub fn new<F: IntoForwardTime>(
         graph: demes::Graph,
         burnin_time: F,
         rounding: Option<demes::RoundTimeToInteger>,
@@ -24,7 +97,47 @@ impl ForwardGraph {
             Some(r) => graph.to_integer_generations(r)?,
             None => graph.to_generations()?,
         };
-        Ok(Self { graph })
+        let model_times = ModelTime::new_from_graph(burnin_time, &graph)?;
+        let child_epochs = Option::<CurrentEpochs>::default();
+        let parent_epochs = Option::<CurrentEpochs>::default();
+        Ok(Self {
+            graph,
+            model_times,
+            child_epochs,
+            parent_epochs,
+        })
+    }
+
+    // NOTE: is this a birth time or a parental time?
+    // Semantically, we want this function to:
+    // * Update BOTH parental and offspring info.
+    //   * What epoch are we in for both parent/offspring demes?
+    //   * What size is each parental/offspring deme?
+    // * with respect to offspring demes:
+    //   * What Pulse events are happening "now"?
+    //   * What migrations are going on?
+    // NOTE: we are going w/the decision to make
+    // this a PARENTAL generation time.
+    //
+    // NOTE: to improve performance we may
+    // * cache the index of the current Epoch for each extant Deme
+    // * Use that index as for Iterator::skip()
+    // * Doing so will break some of our existing tests?
+    pub fn update_state<F: IntoForwardTime>(
+        &mut self,
+        parental_generation_time: F,
+    ) -> Result<(), DemesForwardError> {
+        let parental_generation_time = parental_generation_time.into();
+        self.update_parental_deme_epochs(parental_generation_time)?;
+        self.update_child_deme_epochs(parental_generation_time)
+    }
+
+    pub fn parental_epochs(&self) -> Option<&CurrentEpochs> {
+        self.parent_epochs.as_ref()
+    }
+
+    pub fn child_epochs(&self) -> Option<&CurrentEpochs> {
+        self.child_epochs.as_ref()
     }
 }
 
@@ -63,9 +176,34 @@ demes:
     }
 
     #[test]
-    fn initialize_graph() {
+    fn one_deme_two_epochs() {
         let demes_graph = two_epoch_model();
-        ForwardGraph::new(demes_graph, 100_u32, None).unwrap();
+        let mut graph = ForwardGraph::new(demes_graph, 100_u32, None).unwrap();
+        graph.update_state(125_i32).unwrap();
+        assert_eq!(graph.parental_epochs().unwrap().iter().count(), 1);
+        for epoch in graph.parental_epochs().unwrap().iter() {
+            // Access the underlying demes::Time
+            assert_eq!(epoch.end_time(), 0.0);
+        }
+        graph.update_state(75_i32).unwrap();
+        assert_eq!(graph.parental_epochs().unwrap().iter().count(), 1);
+        for epoch in graph.parental_epochs().unwrap().iter() {
+            // Access the underlying demes::Time
+            assert_eq!(epoch.end_time(), 50.0);
+        }
+
+        // The last generation
+        graph.update_state(150_i32).unwrap();
+        for epoch in graph.parental_epochs().unwrap().iter() {
+            // Access the underlying demes::Time
+            assert_eq!(epoch.end_time(), 0.0);
+        }
+        assert!(graph.child_epochs().is_none());
+
+        // One past the last generation
+        graph.update_state(151_i32).unwrap();
+        assert!(graph.parental_epochs().is_none());
+        assert!(graph.child_epochs().is_none());
     }
 
     #[test]
--- src/time.rs
+++ src/time.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Copy, Clone)]
+use crate::DemesForwardError;
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub struct ForwardTime(f64);
 
 impl ForwardTime {
@@ -9,6 +11,10 @@ impl ForwardTime {
     pub fn new<F: Into<ForwardTime>>(value: F) -> Self {
         value.into()
     }
+
+    pub fn value(&self) -> f64 {
+        self.0
+    }
 }
 
 impl<T> From<T> for ForwardTime
@@ -17,5 +23,159 @@ where
 {
     fn from(value: T) -> Self {
         Self(value.into())
+    }
+}
+
+pub trait IntoForwardTime: Into<ForwardTime> + std::fmt::Debug + Copy {}
+
+impl<T> IntoForwardTime for T where T: Into<ForwardTime> + std::fmt::Debug + Copy {}
+
+pub struct ModelTime {
+    model_start_time: demes::Time,
+    model_duration: f64,
+    burnin_generation: f64,
+}
+
+impl ModelTime {
+    pub(crate) fn convert(
+        &self,
+        time: ForwardTime,
+    ) -> Result<Option<demes::Time>, DemesForwardError> {
+        if time.value() < self.model_duration + self.burnin_generation {
+            Ok(Some(
+                (self.burnin_generation + self.model_duration - 1.0 - time.value()).into(),
+            ))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+fn get_model_start_time(graph: &demes::Graph) -> demes::Time {
+    // first end time of all demes with start time of infinity
+    let mut times = graph
+        .demes()
+        .iter()
+        .filter(|deme| deme.start_time() == f64::INFINITY)
+        .map(|deme| deme.epochs()[0].end_time())
+        .collect::<Vec<_>>();
+    // NOTE: ends.is_empty() is an Error here!!!
+
+    // start times of all demes whose start time is not infinity
+    times.extend(
+        graph
+            .demes()
+            .iter()
+            .filter(|deme| deme.start_time() != f64::INFINITY)
+            .map(|deme| deme.start_time()),
+    );
+
+    times.extend(
+        graph
+            .migrations()
+            .iter()
+            .filter(|migration| migration.start_time() != f64::INFINITY)
+            .map(|migration| migration.start_time()),
+    );
+
+    times.extend(
+        graph
+            .migrations()
+            .iter()
+            .filter(|migration| migration.start_time() != f64::INFINITY)
+            .map(|migration| migration.end_time()),
+    );
+
+    times.extend(graph.pulses().iter().map(|pulse| pulse.time()));
+
+    debug_assert!(!times.is_empty());
+
+    demes::Time::from(f64::from(*times.iter().max().unwrap()) + 1.0)
+}
+
+impl ModelTime {
+    pub(crate) fn new_from_graph(
+        burnin_time_length: crate::ForwardTime,
+        graph: &demes::Graph,
+    ) -> Result<Self, crate::DemesForwardError> {
+        // The logic here is lifted from the fwdpy11
+        // demes import code by Aaron Ragsdale.
+        let most_ancient_deme_start = graph
+            .demes()
+            .iter()
+            .map(|deme| deme.start_time())
+            .collect::<Vec<_>>()
+            .into_iter()
+            .max()
+            .unwrap();
+
+        // Thus MUST be true!
+        let model_start_time = get_model_start_time(graph);
+
+        let most_recent_deme_end = graph
+            .demes()
+            .iter()
+            .map(|deme| deme.end_time())
+            .collect::<Vec<_>>()
+            .into_iter()
+            .min()
+            .unwrap();
+        let model_duration = if most_recent_deme_end > 0.0 {
+            f64::from(model_start_time) - f64::from(most_recent_deme_end)
+        } else {
+            f64::from(model_start_time)
+        };
+
+        let burnin_generation = burnin_time_length.value();
+        Ok(Self {
+            model_start_time,
+            model_duration,
+            burnin_generation,
+        })
+    }
+}
+
+// FIXME: delete at some point
+// when we have a full public API in place.
+// These tests tests NON-PUBLIC bits
+// of the API. They are for design musings
+// only and should be deleted prior to merge.
+#[cfg(test)]
+mod delete_before_merge {
+    use super::*;
+
+    fn two_epoch_model() -> demes::Graph {
+        let yaml = "
+time_units: generations
+demes:
+ - name: A
+   epochs:
+    - start_size: 200
+      end_time: 50
+    - start_size: 100
+";
+        demes::loads(yaml).unwrap()
+    }
+
+    #[test]
+    fn test_forwards_to_backwards_time_conversion() {
+        let g = two_epoch_model();
+        let graph = crate::graph::ForwardGraph::new(g, 100, None).unwrap();
+        assert_eq!(
+            graph
+                .model_times
+                .convert(ForwardTime::from(0))
+                .unwrap()
+                .unwrap(),
+            150.
+        );
+        assert_eq!(
+            graph
+                .model_times
+                .convert(ForwardTime::from(150.))
+                .unwrap()
+                .unwrap(),
+            0.
+        );
     }
 }
